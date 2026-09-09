9eca2268c0d4d1bdde91f870abf7843fc6c57541
xushnudraxmon03-chik <xushnudraxmon03@gmail.com>
2026-09-09 15:36:18 +0500

    Add public navbar design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Navbar from "./components/public/navbar"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="min-h-screen bg-slate-50">
+      <Navbar />
+      <main id="home" className="mx-auto max-w-7xl px-5 py-20 lg:px-8">
+        <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-slate-950 sm:text-6xl">
+          Yaxshi mahsulotlar qolmasin.
+        </h1>
+      </main>
+    </div>
   )
 }
 
